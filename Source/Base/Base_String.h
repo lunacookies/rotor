176623cedbfa47d3f991643838be8400b3637110
Luna Razzaghipour <luna@xoria.org>
2024-04-30 22:13:00 +1000

    Implement hierarchical view structure

--- Source/Base/Base_String.h
+++ Source/Base/Base_String.h
@@ -6,5 +6,6 @@ struct String8
 };
 
 #define Str8Lit(s) ((String8){.data = (U8 *)(s), .count = sizeof(s) - 1})
+#define Str8VArg(s) (S32)(s).count, (s).data
 
 function B32 String8Match(String8 a, String8 b);
--- Source/Rotor/Rotor_MainView.m
+++ Source/Rotor/Rotor_MainView.m
@@ -61,6 +61,10 @@ typedef struct View View;
 struct View
 {
 	View *next;
+	View *first;
+	View *last;
+	View *parent;
+
 	View *next_all;
 	View *prev_all;
 
@@ -69,6 +73,7 @@ struct View
 	V2 origin_target;
 	V2 size;
 	V2 padding;
+	F32 child_gap;
 	V3 color;
 	V3 pressed_color;
 	B32 pressed;
@@ -96,8 +101,8 @@ struct Event
 typedef struct State State;
 struct State
 {
-	View *first_view;
-	View *last_view;
+	View *root;
+	View *current;
 
 	View *first_view_all;
 	View *last_view_all;
@@ -108,6 +113,8 @@ struct State
 	GlyphAtlas *glyph_atlas;
 	CTFontRef font;
 	U64 build_index;
+
+	B32 make_next_current;
 };
 
 function void
@@ -188,6 +195,18 @@ StateInit(State *state, GlyphAtlas *glyph_atlas)
 	state->font = (__bridge CTFontRef)[NSFont systemFontOfSize:14 weight:NSFontWeightRegular];
 }
 
+function void
+MakeNextCurrent(State *state)
+{
+	state->make_next_current = 1;
+}
+
+function void
+MakeParentCurrent(State *state)
+{
+	state->current = state->current->parent;
+}
+
 function View *
 ViewAlloc(State *state)
 {
@@ -246,17 +265,29 @@ ViewRelease(State *state, View *view)
 function void
 ViewPush(State *state, View *view)
 {
-	if (state->first_view == 0)
+	view->next = 0;
+	view->first = 0;
+	view->last = 0;
+	view->parent = 0;
+
+	// Push view onto the list of children of the current view.
+	view->parent = state->current;
+	if (state->current->first == 0)
 	{
-		state->first_view = view;
+		Assert(state->current->last == 0);
+		state->current->first = view;
 	}
 	else
 	{
-		state->last_view->next = view;
+		state->current->last->next = view;
 	}
+	state->current->last = view;
 
-	view->next = 0;
-	state->last_view = view;
+	if (state->make_next_current)
+	{
+		state->current = view;
+		state->make_next_current = 0;
+	}
 }
 
 function View *
@@ -384,10 +415,16 @@ Button(State *state, String8 string)
 function Signal
 Checkbox(State *state, B32 *value, String8 string)
 {
+	MakeNextCurrent(state);
 	View *view = ViewFromKey(state, string);
 	view->flags |= ViewFlags_DrawBackground;
-	view->padding.x = 10;
-	view->padding.y = 10;
+	view->padding.x = 5;
+	view->padding.y = 5;
+
+	View *mark = ViewFromKey(state, Str8Lit("checkboxchild"));
+	mark->flags |= ViewFlags_DrawBackground;
+	mark->padding.x = 5;
+	mark->padding.y = 5;
 
 	Signal signal = SignalForView(state, view);
 	if (Clicked(signal))
@@ -403,6 +440,9 @@ Checkbox(State *state, B32 *value, String8 string)
 		view->pressed_color.r = 0.2f;
 		view->pressed_color.g = 0.7f;
 		view->pressed_color.b = 1;
+		mark->color.r = 1;
+		mark->color.g = 1;
+		mark->color.b = 1;
 	}
 	else
 	{
@@ -412,8 +452,12 @@ Checkbox(State *state, B32 *value, String8 string)
 		view->pressed_color.r = 0.4f;
 		view->pressed_color.g = 0.4f;
 		view->pressed_color.b = 0.4f;
+		mark->color.r = 0.5f;
+		mark->color.g = 0.5f;
+		mark->color.b = 0.5f;
 	}
 
+	MakeParentCurrent(state);
 	return signal;
 }
 
@@ -434,45 +478,63 @@ PruneUnusedViews(State *state)
 }
 
 function void
+LayoutView(State *state, View *view, V2 *current_position)
+{
+	MemoryZeroStruct(&view->rasterized_line);
+	if (view->flags & ViewFlags_DrawText)
+	{
+		RasterizeLine(state->arena, &view->rasterized_line, view->string,
+		        state->glyph_atlas, state->font);
+	}
+
+	view->origin_target = *current_position;
+	if (view->flags & ViewFlags_FirstFrame)
+	{
+		view->origin = *current_position;
+	}
+	else
+	{
+		view->origin.x += (view->origin_target.x - view->origin.x) * 0.1f;
+		view->origin.y += (view->origin_target.y - view->origin.y) * 0.1f;
+	}
+
+	V2 start_position = *current_position;
+	current_position->x += view->padding.x;
+	current_position->y += view->padding.y;
+
+	current_position->y += RoundF32(view->rasterized_line.bounds.y);
+
+	F32 content_width = RoundF32(view->rasterized_line.bounds.x);
+
+	for (View *child = view->first; child != 0; child = child->next)
+	{
+		LayoutView(state, child, current_position);
+		current_position->y += view->child_gap;
+		content_width = Max(content_width, child->size.x);
+	}
+
+	current_position->y += view->padding.y;
+	current_position->x = start_position.x;
+
+	view->size.x = content_width + view->padding.x * 2;
+	view->size.y = current_position->y - start_position.y;
+}
+
+function void
 LayoutUI(State *state)
 {
 	V2 current_position = {0};
-	current_position.x = 20;
-	current_position.y = 20;
-	F32 margin = 10;
-
-	for (View *view = state->first_view; view != 0; view = view->next)
-	{
-		MemoryZeroStruct(&view->rasterized_line);
-		if (view->flags & ViewFlags_DrawText)
-		{
-			RasterizeLine(state->arena, &view->rasterized_line, view->string,
-			        state->glyph_atlas, state->font);
-		}
-
-		view->origin_target = current_position;
-		if (view->flags & ViewFlags_FirstFrame)
-		{
-			view->origin = current_position;
-		}
-		else
-		{
-			view->origin.x += (view->origin_target.x - view->origin.x) * 0.1f;
-			view->origin.y += (view->origin_target.y - view->origin.y) * 0.1f;
-		}
-
-		view->size.x = RoundF32(view->rasterized_line.bounds.x) + view->padding.x * 2;
-		view->size.y = RoundF32(view->rasterized_line.bounds.y) + view->padding.y * 2;
-
-		current_position.y += view->size.y + margin;
-	}
+	LayoutView(state, state->root, &current_position);
 }
 
 function void
 StartBuild(State *state)
 {
-	state->first_view = 0;
-	state->last_view = 0;
+	state->root = ViewAlloc(state);
+	state->root->padding.x = 20;
+	state->root->padding.y = 20;
+	state->root->child_gap = 10;
+	state->current = state->root;
 	state->build_index++;
 }
 
@@ -520,67 +582,75 @@ BuildUI(State *state)
 }
 
 function void
-RenderUI(State *state, BoxArray *box_array)
+RenderView(State *state, View *view, BoxArray *box_array)
 {
-	for (View *view = state->first_view; view != 0; view = view->next)
+	if (view->flags & ViewFlags_DrawBackground)
 	{
-		if (view->flags & ViewFlags_DrawBackground)
+		Box *bg_box = box_array->boxes + box_array->count;
+		box_array->count++;
+
+		bg_box->origin = view->origin;
+		bg_box->size = view->size;
+
+		if (view->pressed)
 		{
-			Box *bg_box = box_array->boxes + box_array->count;
-			box_array->count++;
-
-			bg_box->origin = view->origin;
-			bg_box->size = view->size;
-
-			if (view->pressed)
-			{
-				bg_box->color = view->pressed_color;
-			}
-			else
-			{
-				bg_box->color = view->color;
-			}
+			bg_box->color = view->pressed_color;
 		}
-
-		if (view->flags & ViewFlags_DrawText)
+		else
 		{
-			V2 text_origin = view->origin;
-			text_origin.x += view->padding.x;
-			text_origin.y += view->padding.y;
-			text_origin.y += (view->rasterized_line.bounds.y +
-			                         (F32)CTFontGetCapHeight(state->font)) *
-			                 0.5f;
+			bg_box->color = view->color;
+		}
+	}
 
-			for (U64 glyph_index = 0; glyph_index < view->rasterized_line.glyph_count;
-			        glyph_index++)
+	if (view->flags & ViewFlags_DrawText)
+	{
+		V2 text_origin = view->origin;
+		text_origin.x += view->padding.x;
+		text_origin.y += view->padding.y;
+		text_origin.y +=
+		        (view->rasterized_line.bounds.y + (F32)CTFontGetCapHeight(state->font)) *
+		        0.5f;
+
+		for (U64 glyph_index = 0; glyph_index < view->rasterized_line.glyph_count;
+		        glyph_index++)
+		{
+			V2 position = view->rasterized_line.positions[glyph_index];
+			position.x += text_origin.x;
+			position.y += text_origin.y;
+
+			GlyphAtlasSlot *slot = view->rasterized_line.slots[glyph_index];
+
+			Box *box = box_array->boxes + box_array->count;
+			box_array->count++;
+			Assert(box_array->count <= box_array->capacity);
+
+			box->origin = position;
+			box->texture_origin.x = slot->origin.x;
+			box->texture_origin.y = slot->origin.y;
+			box->size.x = slot->size.x;
+			box->size.y = slot->size.y;
+			box->texture_size.x = slot->size.x;
+			box->texture_size.y = slot->size.y;
+
+			if (!view->pressed)
 			{
-				V2 position = view->rasterized_line.positions[glyph_index];
-				position.x += text_origin.x;
-				position.y += text_origin.y;
-
-				GlyphAtlasSlot *slot = view->rasterized_line.slots[glyph_index];
-
-				Box *box = box_array->boxes + box_array->count;
-				box_array->count++;
-				Assert(box_array->count <= box_array->capacity);
-
-				box->origin = position;
-				box->texture_origin.x = slot->origin.x;
-				box->texture_origin.y = slot->origin.y;
-				box->size.x = slot->size.x;
-				box->size.y = slot->size.y;
-				box->texture_size.x = slot->size.x;
-				box->texture_size.y = slot->size.y;
-
-				if (!view->pressed)
-				{
-					box->color.r = 1;
-					box->color.g = 1;
-					box->color.b = 1;
-				}
+				box->color.r = 1;
+				box->color.g = 1;
+				box->color.b = 1;
 			}
 		}
 	}
+
+	for (View *child = view->first; child != 0; child = child->next)
+	{
+		RenderView(state, child, box_array);
+	}
+}
+
+function void
+RenderUI(State *state, BoxArray *box_array)
+{
+	RenderView(state, state->root, box_array);
 }
 
 @implementation MainView
